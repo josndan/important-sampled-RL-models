02b42fe5f932f21c662be28fe3e83acbf9da1972
Joseph Daniel Selvaraaj <joedanselva@outlook.com>
2022-08-03 00:06:27 -0400

    Change the stored history structure

--- experiment.py
+++ experiment.py
@@ -16,39 +16,37 @@ class Simulator:
         history = []
         t = 0
         first_reward = 0
-        if isinstance(agent, AgentOnObservation):
-            while not self.world.reached_absorbing():
+        while not self.world.reached_absorbing():
+            current_state = self.world.get_current_state()
+
+            if isinstance(agent, AgentOnObservation):
                 current_observation = self.world.get_current_observation()
-                current_state = self.world.get_current_state()
                 next_action = agent.get_action(current_observation)
-                reward = self.world.take_action(next_action)
-                if t != 0:
-                    epi_return += current_discount * reward
-                    current_discount *= discount
-                else:
-                    epi_return += reward
-                    first_reward = reward
-                history.append((current_state, current_observation, next_action, reward))
-                t += 1
-        else:
-            while not self.world.reached_absorbing():
-                current_state = self.world.get_current_state()
+            else:
                 next_action = agent.get_action(current_state)
-                reward = self.world.take_action(next_action)
-                if t != 0:
-                    epi_return += current_discount * reward
-                    current_discount *= discount
-                else:
-                    epi_return += reward
-                    first_reward = reward
-                if isinstance(self.world, POMDP):
-                    current_observation = self.world.get_current_observation()
-                    history.append((current_state, current_observation, next_action, reward))
-                else:
-                    history.append((current_state, next_action, reward))
-                t += 1
-        if first_reward!=epi_return:
-            5
+
+            reward = self.world.take_action(next_action)
+            if t != 0:
+                epi_return += current_discount * reward
+                current_discount *= discount
+            else:
+                epi_return += reward
+                first_reward = reward
+
+            if isinstance(self.world, POMDP):
+                current_observation = self.world.get_current_observation()
+                history.append([current_state, current_observation, next_action, str(reward)])
+            else:
+                history.append([current_state, next_action, str(reward)])
+
+            t += 1
+
+        current_state = self.world.get_current_state()
+        if isinstance(self.world, POMDP):
+            current_observation = self.world.get_current_observation()
+            history.append([current_state, current_observation])
+        else:
+            history.append([current_state])
         return history, epi_return, first_reward
 
 
@@ -67,9 +65,11 @@ class Experiment(Simulator):
             first_reward += f_reward
             if self.plot:
                 points.append((i, estimated_return / i))
-                plt.xlabel("Number of episodes")
-                plt.ylabel("Estimated Return")
-                plt.plot(*list(zip(*points)))
-                plt.show()
+
+        if self.plot:
+            plt.xlabel("Number of episodes")
+            plt.ylabel("Estimated Return")
+            plt.plot(*list(zip(*points)))
+            plt.show()
 
         return first_reward/num_episode, estimated_return / num_episode
